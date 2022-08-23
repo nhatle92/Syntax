ede093ad435ace226966cd1cbe70f0bcb002df73
nhatle <lenhat0905@gmail.com>
2022-08-23 19:03:28 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,32 @@
+package basic.dev;
+/**
+ * Ghi chú (comment nhiều dòng)
+ * @author TranThanhBinh
+ * 1. Tên Lớp trùng tên file
+ * 2. Tên lớp chữ cái đầu viết hoa. Nếu từ ghép thì những chữ cái đầu viết hoa
+ * 4. Tên lớp đặt phải có ý nghĩa. Ví dụ: class Peson : lớp mô tả con người
+ * - Interface: Phải có tiền tố I. Ví dụ: IListener
+ * - Abstract: Phải có tiền tố Abstract,. Ví dụ: AbstractShape
+ */
+public class MainApp {
+//Comment 1 dòng cách 1. Phím nóng Ctr + /
+	/*Comment 1 dòng cách 2*/
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+		// TODO Auto-generated method stub
+		/**
+		 * Cách đặt tên biến:
+		 * 1. Không chứa ký tự đặc biệt !@#$%^&*()_+
+		 * 2. Không chứa khoảng trắng
+		 * 3. Viết thường
+		 * 4. Không được bắt đầu bằng số
+		 */
+		int number;
+		String nameSchool;
+	}
+	/*
+	 * tên phương thức tuân theo quy tắc camelCase
+	 */
+	public void show() {
+	}	
+}
